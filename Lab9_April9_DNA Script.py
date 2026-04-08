da92c64061bd6a690111bae3f17cf714067b7fcc
Anu Panthy <apanthyofficial@gmail.com>
2026-04-08 19:57:10 -0400

    Add files via upload

---
+++ Lab9_April9_DNA Script.py
@@ -0,0 +1,47 @@
+"""
+Script Name: dna_script.py
+Author: Anu Panthy
+Description:This script creates a DNA sequence variable
+"""
+#####################################################################
+#Activity1
+# Step 2: Print Variable for a short DNA sequence
+dna_sequence = "ATGCGTAC"
+
+# Step 3: Create another variable for a number
+sample_number = 5
+
+# Step 4: Add comments and print functions
+
+# Print the DNA sequence
+print("dna_sequence")
+
+# Print the number variable
+print("sample_number")
+
+weekdays=['Mo','Tue', 'Wed']
+weekdays[2]
+
+weekdays.append('Sa')
+print(weekdays)
+
+weekdays.remove('Mo')
+print(weekdays)
+
+#####################################################################
+
+#Activity2
+#create a list of three fav colors
+colors = ['White', 'Navy_Blue', 'Light_Orange']
+
+#Print the second color from the list.
+print(colors[1])
+
+#Add a new color to the list
+colors.append('Purple')
+
+#Remove one color from the list
+colors.remove('White')
+
+#Print the final list
+print(colors)
