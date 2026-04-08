e50093bd52dd656a5d72409e7c04a1be621e0e1b
Anu Panthy <apanthyofficial@gmail.com>
2026-04-08 19:58:11 -0400

    Add files via upload

---
+++ Lab 9-Batch Mode.py
@@ -0,0 +1,9 @@
+"""
+Hello.py
+Purpose: This progress demonstrates the use of a basic print function.
+Author: Anu Panthy
+Date: April7, 2026
+"""
+
+# Print a friendly message to the console
+print ( "'Hello!"
